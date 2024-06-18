ce6665aa750eccfd30e8bd08914dadf8aabc0bfb
nasgor <mn9603379@gmail.com>
2024-06-18 20:10:32 +0700

    Update Google Sheet.js

--- assets/js/Google Sheet.js
+++ assets/js/Google Sheet.js
@@ -1,11 +1,31 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycby7uE1HGaPxqjkgsAYNlvWAF6eSSiI2YLAydmEHl2w2zi1k6dykN-PEsoINNvajzzM7/exec'
+document.getElementById('contact-form').addEventListener('submit', submitForm);
 
-const form = document.forms['contact-form']
+function submitForm(e) {
+  e.preventDefault();
 
-form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Thank you! your form is submitted successfully." ))
-  .then(() => { window.location.reload(); })
-  .catch(error => console.error('Error!', error.message))
-})
+  const nama = document.getElementById('nama').value;
+  const email = document.getElementById('email').value;
+  const pesan = document.getElementById('pesan').value;
+
+  fetch('https://script.google.com/macros/s/AKfycbymF5cb0q_8AD96hqQw6DbO9uMdv7KS549RVkN6r8e8C49Kkxi9uncw6ze-P6NrKktT6w/exec', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: `nama=${encodeURIComponent(nama)}&email=${encodeURIComponent(email)}&pesan=${encodeURIComponent(pesan)}`
+  })
+  .then(response => response.json())
+  .then(data => {
+    if (data.result === 'success') {
+      document.getElementById('status').innerText = 'Message sent successfully!';
+    } else {
+      document.getElementById('status').innerText = 'Error sending message!';
+    }
+  })
+  .catch(error => {
+    document.getElementById('status').innerText = 'Error sending message!';
+    console.error('Error:', error);
+  });
+
+  document.getElementById('contact-form').reset();
+}
